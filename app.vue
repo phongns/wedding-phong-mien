6414497f1bb0d5c0f7e43070024a9cf25b0c57c9
phongns <nguyensiphong92@gmail.com>
2023-10-16 22:46:23 +0700

    fix width

--- app.vue
+++ app.vue
@@ -15,7 +15,7 @@ useHead({
           as="style">
     <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&amp;family=Jura:wght@400;700&amp;family=Philosopher:wght@400;700&amp;family=Arima:wght@400;700&amp;family=Athiti:wght@400;700&amp;family=Tinos:wght@400;700&amp;family=Thasadith:wght@400;700&amp;display=swap" as="style"> -->
     <div class="main-picture">
-      <img :src="data.mainPicture" alt="ảnh cưới" loading="lazy" class="w-100" />
+      <img :src="data.mainPicture" alt="ảnh cưới" loading="lazy" />
       <span class="bg-divider bg-overlay">
         <svg xmlns="http://www.w3.org/2000/svg" class="divider" viewBox="0 0 1024 160" preserveAspectRatio="none">
           <g class="divider-g">
@@ -180,15 +180,22 @@ html {
   width: 100% !important;
 }
 
+img {
+  width: 100%;
+  height: auto;
+  -o-object-fit: cover;
+  object-fit: cover;
+}
+
 .main-picture {
   display: flex;
   justify-content: center;
   position: relative;
   background-image: radial-gradient(circle at 13% 47%, rgba(140, 140, 140, 0.03) 0%, rgba(140, 140, 140, 0.03) 25%, transparent 25%, transparent 100%), radial-gradient(circle at 28% 63%, rgba(143, 143, 143, 0.03) 0%, rgba(143, 143, 143, 0.03) 16%, transparent 16%, transparent 100%), radial-gradient(circle at 81% 56%, rgba(65, 65, 65, 0.03) 0%, rgba(65, 65, 65, 0.03) 12%, transparent 12%, transparent 100%), radial-gradient(circle at 26% 48%, rgba(60, 60, 60, 0.03) 0%, rgba(60, 60, 60, 0.03) 6%, transparent 6%, transparent 100%), radial-gradient(circle at 97% 17%, rgba(150, 150, 150, 0.03) 0%, rgba(150, 150, 150, 0.03) 56%, transparent 56%, transparent 100%), radial-gradient(circle at 50% 100%, rgba(25, 25, 25, 0.03) 0%, rgba(25, 25, 25, 0.03) 36%, transparent 36%, transparent 100%), radial-gradient(circle at 55% 52%, rgba(69, 69, 69, 0.03) 0%, rgba(69, 69, 69, 0.03) 6%, transparent 6%, transparent 100%), linear-gradient(90deg, rgb(255, 255, 255), rgb(255, 255, 255));
 
-  img {
-    max-width: 600px;
-  }
+  // img {
+  //   max-width: 600px;
+  // }
 }
 
 .divider {
